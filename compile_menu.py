f4f8370a4451112eaf08a81d481ebf92db1fac90
Dave Moffat <me@davemoffat.com>
2020-02-17 21:59:33 +0000

    Major refactor - no longer using amount_units function - now using pint unit registry function, redefining base unit objects and catching if defined in unit registry, unit registry now defined in text file and read in

--- compile_menu.py
+++ compile_menu.py
@@ -9,37 +9,8 @@ from pint import UnitRegistry
 from argparse import ArgumentParser
 
 ureg = UnitRegistry()
-# Some definitions
-ureg.define('carrot = 72 grams')
-ureg.define('onion = 90 grams')
-ureg.define('red_onion = 90 grams')
-ureg.define('roll = 60 grams')
-ureg.define('vege_burger = 125 grams')
-ureg.define('bay_leaf = 5 grams')
-ureg.define('thyme_sprig = 5 grams')
-ureg.define('turnip = 500 grams')
-ureg.define('parsnip = 112 grams')
-ureg.define('garlic_clove = 7 grams')
-ureg.define('egg_large = 70 grams')
-ureg.define('banana = 183 grams')
-ureg.define('apple = 85 grams')
-ureg.define('tea_bag = 2 grams')
-ureg.define('leek = 200 grams')
-ureg.define('can = 400 grams')
-ureg.define('lettuce = 400 grams')
-ureg.define('large_jar = 800 grams')
-ureg.define('tomato = 62 grams')
-ureg.define('slice = 10 grams')
-ureg.define('potato = 150 grams')
-ureg.define('garlic_bulb = 9 garlic_clove')
-ureg.define('bread_loaf = 8 bread_slice')
-ureg.define('celery = 16 celery_stick')
-ureg.define('pinch = 3 grams')
-
+ureg.load_definitions('unit_registry.txt')
 # 1 tsp smoked paprika = 3.3g
-# 9 cloves garlic in a bulb
-
-DEBUG = True
 
 def get_args():
     """
@@ -87,26 +58,12 @@ class Ingredient():
     if needed"""
     
 
-    def __init__(self, name, value, unit):
+    def __init__(self, name, quantity):
         self.name = name
+        self.quantity = quantity
 
-        if len(unit) > 0:
-            self.value = value * ureg.parse_expression(unit)
-        else:
-            print("using " + name + " as unit")
-            self.value = value * ureg.parse_expression(name)
-
-    def add(self, value, unit):
-        if DEBUG:
-            # pdb.set_trace()
-            print((value,unit))
-            print("adding " + self.name + " with unit " + unit + " and existing unit" + str(self.value.units))
-
-        if len(unit) > 0:
-            self.value = self.value + (value * ureg.parse_expression(unit))
-        else:
-            print("using " + self.name + " as unit")
-            self.value = self.value + (value * ureg.parse_expression(self.name))
+    def add(self, quantity):
+        self.quantity = self.quantity + quantity
 
     def pr(self):
         return '{!s}'.format(self.value)
@@ -129,17 +86,18 @@ class Ingredients():
     def __init__(self):
         self.list = dict()
 
-    def add(self, ingredient, value, unit):
+    def add(self, ingredient, quantity):
         ingredient = self.equivalent_check(ingredient)
+        print(ingredient)
         if ingredient in self.list:
             #try:
             a = self.list[ingredient]
-            print("Adding %s %s: %s" % (value, unit, ingredient))
-            a.add(value, unit)
+            print("Adding {!s}: {!s}".format(quantity, ingredient))
+            a.add(quantity)
             self.list[ingredient] = a
         else:
-            print("Adding %s %s: %s" % (value, unit, ingredient))
-            self.list[ingredient] = Ingredient(ingredient, value, unit)
+            print("Adding {!s}: {!s}".format(quantity, ingredient))
+            self.list[ingredient] = Ingredient(ingredient, quantity)
 
     def equivalent_check(self, ingredient):
         for equivalent in self.equi:
@@ -158,9 +116,9 @@ class Ingredients():
                 this_shop = self.default_shop
 
             if this_shop in self.shops:
-                self.shops[this_shop] = self.shops[this_shop] + "    [ ] {!s} {!s}\n".format(round(item.value,2), item.name)
+                self.shops[this_shop] = self.shops[this_shop] + "    [ ] {:~P} {!s}\n".format(item.quantity, item.name)
             else:
-                self.shops[this_shop] = "    [ ] {!s} {!s}\n".format(item.value, item.name)
+                self.shops[this_shop] = "    [ ] {:~P} {!s}\n".format(item.quantity, item.name)
 
         for shop in self.shops:
             output +=  "## %s\n" % (shop)
@@ -174,25 +132,31 @@ def process_menu(menu_yaml, outdir, pdf_generation):
     menu = open_yaml(menu_yaml)
     os.makedirs(outdir, exist_ok = True)
 
-    for item in menu:
+    for recipe_name in menu:
         skip = False
         try:
-            file = "recipe/" + item + ".yaml"
+            file = "recipe/" + recipe_name + ".yaml"
             recipe = open_yaml(file)
-        except:
+        except FileNotFoundError:
             # not very elegant
-            print("Can't open %s as recipe... trying as single ingredient" % item)
-            (number, unit) = amount_units(menu[item])
-            print("%20s: %8.2f %s" % (item, float(number), unit))
-            ingredients.add(item, float(number), unit)
-            print("skip ingredients")
+            print("Can't open %s as recipe... trying as single ingredient" % recipe_name)
+            if isinstance(menu[recipe_name], (int, float)):
+                try:
+                    amount = ureg(str(menu[recipe_name])+recipe_name)
+                except:
+                    amount = ureg(str(menu[recipe_name])+'quantity')
+            else:
+                amount = ureg(menu[recipe_name])
+
+            print('{!s}: {!s}\n'.format(recipe_name, amount))
+            ingredients.add(recipe_name, amount)
             skip = True
             continue
 
         if not skip:
-            for day in menu[item]:
-                people = int(menu[item][day])
-                mdname = os.path.basename(item + ".md")
+            for day in menu[recipe_name]:
+                people = int(menu[recipe_name][day])
+                mdname = os.path.basename(recipe_name + ".md")
                 if day != 'people':
                     mdname = day + '_' + mdname
                     print(mdname)
@@ -212,15 +176,25 @@ def process_menu(menu_yaml, outdir, pdf_generation):
                 f.write("### Ingredients: \n")
                 f.write("\n")
                 for ingredient in recipe['ingredients']:
-                    amount = recipe['ingredients'][ingredient]
-                    (number, unit) = amount_units(amount)
-                    number = float(number) / serves
-                    f.write("%28s: %8.2f %s\n" % (ingredient, float(number) * people, unit))
-                    ingredients.add(ingredient, float(number) * people, unit)
+                    # There must be a better way of testing whether or not an item has been defined
+                    if isinstance(recipe['ingredients'][ingredient], (int, float)):
+                        try:
+                            print('trying to use predefined item')
+                            amount = ureg(str(recipe['ingredients'][ingredient])+ingredient)
+                        except: 
+                            print('failed - defining new item')
+                            # Currently set undefined items to a 'quantity' rather than the individual defined item
+                            amount = ureg(str(recipe['ingredients'][ingredient])+'quantity')
+                    else:
+                        amount = ureg(recipe['ingredients'][ingredient])
+
+                    amount_pp = amount/serves
+                    f.write('\t{!s}: {!s}\n'.format(ingredient, amount_pp * people))
+                    ingredients.add(ingredient, amount_pp * people)
 
                 if 'method' in recipe:
                     try:
-                        f.write("### Description\n")
+                        f.write("\n\n### Description\n")
                         f.write(recipe['method'])#.encode('utf-8'))
                         f.write("\n")
                     except:
